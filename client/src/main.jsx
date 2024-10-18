5892108bf39156e0799b6ccef2d368ed89157fd0
David Chay <yapperdark2@gmail.com>
2024-10-17 21:54:08 -0600

    Add the components for the login page

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
+import HeaderJustIcon from './components/headerJustIcon/HeaderJustIcon'
+import LoginForm from './components/loginForm/LoginForm'
+import SimpleFooter from './components/simpleFooter/SimpleFooter'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <HeaderJustIcon />
+    <LoginForm />
+    <SimpleFooter />
   </StrictMode>,
 )
